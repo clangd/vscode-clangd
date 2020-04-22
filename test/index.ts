df0c0aae3be56a2cb1e8ddcda0717443597449c1
Sam McCall <sam.mccall@gmail.com>
2020-04-23 00:55:17 +0200

    Less extraneous test files
    Merge the two stages of test bootstrap into one file.
    Make comments more useful.
    Delete dummy extension test.

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,14 @@
+// Entry point for all tests.
+// Spawns VSCode with our extension, and then runs *.test.ts in that context.
+
 import * as glob from 'glob';
 import * as Mocha from 'mocha';
 import * as path from 'path';
+import {runTests} from 'vscode-test';
 
+// The entry point under VSCode - find the test files and run them in Mocha.
 export function run(): Promise<void> {
-  // Create the mocha test
-  const mocha = new Mocha({ui : 'tdd'});
-  mocha.useColors(true);
-
+  const mocha = new Mocha({ui : 'tdd', color: true});
   const testsRoot = path.resolve(__dirname, '..');
 
   return new Promise((c, e) => {
@@ -14,8 +16,6 @@ export function run(): Promise<void> {
       if (err) {
         return e(err);
       }
-
-      // Add files to the test suite
       files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
       try {
@@ -33,3 +33,23 @@ export function run(): Promise<void> {
     });
   });
 }
+
+// The main entry point: its job is to launch VSCode and execute run() under it.
+async function main() {
+  try {
+    // The extension to be loaded is in the parent directory.
+    const extensionDevelopmentPath = path.resolve(__dirname, '../');
+    // The run() function to run in vscode is defined in this file.
+    const extensionTestsPath = __filename;
+    // Download VS Code, unzip it and run the integration test
+    await runTests({extensionDevelopmentPath, extensionTestsPath});
+  } catch (err) {
+    console.error('Failed to run tests');
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  main();
+}
+
