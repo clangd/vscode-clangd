0091369895091d798cce1e0935edea2490e9ac4e
Sam McCall <sam.mccall@gmail.com>
2020-05-03 00:32:56 +0200

    Format

--- src/install.ts
+++ src/install.ts
@@ -23,9 +23,9 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.commands.registerCommand(
-      "clangd.install", async () => { installLatest(context); }));
+      'clangd.install', async () => { installLatest(context); }));
   context.subscriptions.push(
-      vscode.commands.registerCommand("clangd.update", async () => {
+      vscode.commands.registerCommand('clangd.update', async () => {
         const cur =
             vscode.workspace.getConfiguration('clangd').get<string>('path');
         checkUpdates(cur, true, context);
@@ -55,9 +55,9 @@ export async function recover(context: vscode.ExtensionContext) {
     const release = await Github.latestRelease();
     const asset = Github.chooseAsset(release);
     const message =
-        "The clangd language server was not found on your PATH.\n" +
+        'The clangd language server was not found on your PATH.\n' +
         `Would you like to download and install clangd ${release.name}?`;
-    if (await vscode.window.showInformationMessage(message, "Install")) {
+    if (await vscode.window.showInformationMessage(message, 'Install')) {
       const abort = new AbortController();
       try {
         const clangdPath =
@@ -69,8 +69,8 @@ export async function recover(context: vscode.ExtensionContext) {
       }
     }
   } catch (e) {
-    console.error("Auto-install failed: ", e);
-    End.showInstallationHelp("The clangd language server is not installed.");
+    console.error('Auto-install failed: ', e);
+    End.showInstallationHelp('The clangd language server is not installed.');
   }
 }
 
@@ -83,28 +83,28 @@ export async function checkUpdates(clangdPath: string, requested: boolean,
     var asset = Github.chooseAsset(release);
     var upgrade = await Version.upgrade(release, clangdPath);
   } catch (e) {
-    console.log("Failed to check for clangd update: ", e);
+    console.log('Failed to check for clangd update: ', e);
     // We're not sure whether there's an upgrade: stay quiet unless asked.
     if (requested)
       vscode.window.showInformationMessage(
-          "Failed to check for clangd update: " + e);
+          'Failed to check for clangd update: ' + e);
     return;
   }
-  console.log("Checking for clangd update: available=", upgrade.new,
-              " installed=", upgrade.old);
+  console.log('Checking for clangd update: available=', upgrade.new,
+              ' installed=', upgrade.old);
   // Bail out if the new version is better or comparable.
   if (upgrade == null) {
     if (requested)
-      vscode.window.showInformationMessage("clangd is up-to-date");
+      vscode.window.showInformationMessage('clangd is up-to-date');
     return;
   }
   // Ask to update. If this was unsolicited, let the user disable future checks.
-  const message = "An updated clangd language server is available.\n " +
+  const message = 'An updated clangd language server is available.\n ' +
                   `Would you like to upgrade to clangd ${upgrade.new}? (from ${
                       upgrade.old})`;
   const install = `Install clangd ${upgrade.new}`;
-  const dontCheck = "Don't ask again";
-  const opts = requested ? [ install ] : [ install, dontCheck ];
+  const dontCheck = 'Don\'t ask again';
+  const opts = requested ? [install] : [install, dontCheck];
   const response = await vscode.window.showInformationMessage(message, ...opts);
   if (response == install) {
     const abort = new AbortController();
@@ -132,11 +132,11 @@ export interface Asset {
 
 // Fetch the metadata for the latest stable clangd release.
 export async function latestRelease(): Promise<Release> {
-  const url = "https://api.github.com/repos/clangd/clangd/releases/latest";
+  const url = 'https://api.github.com/repos/clangd/clangd/releases/latest';
   const response = await fetch(url);
   if (!response.ok) {
     console.log(response.url, response.status, response.statusText);
-    throw new Error("Can't fetch release: " + response.statusText);
+    throw new Error('Can\'t fetch release: ' + response.statusText);
   }
   return await response.json() as Release;
 }
@@ -144,13 +144,13 @@ export async function latestRelease(): Promise<Release> {
 // Determine which release asset should be installed for this machine.
 export function chooseAsset(release: Github.Release): Github.Asset|null {
   const variants: {[key: string]: string} = {
-    "win32" : "windows",
-    "linux" : "linux",
-    "darwin" : "mac",
+    'win32': 'windows',
+    'linux': 'linux',
+    'darwin': 'mac',
   };
   const variant = variants[os.platform()];
   // 32-bit vscode is still common on 64-bit windows, so don't reject that.
-  if (variant && (os.arch() == "x64" || variant == "windows"))
+  if (variant && (os.arch() == 'x64' || variant == 'windows'))
     return release.assets.find(a => a.name.indexOf(variant) >= 0)
     throw new Error(
         `No clangd ${release.name} binary available for your platform`);
@@ -161,17 +161,17 @@ export function chooseAsset(release: Github.Release): Github.Asset|null {
 namespace End {
 // The configuration is not valid, the user should install clangd manually.
 export async function showInstallationHelp(message: string) {
-  const url = "https://clangd.llvm.org/installation.html";
+  const url = 'https://clangd.llvm.org/installation.html';
   message += `\nSee ${url} for help.`
-  if (await vscode.window.showErrorMessage(message, "Open website"))
+  if (await vscode.window.showErrorMessage(message, 'Open website'))
   vscode.env.openExternal(vscode.Uri.parse(url));
 }
 
 // We tried to install clangd, but something went wrong.
 export function installationFailed(e: any) {
-  console.error("Failed to install clangd: ", e);
+  console.error('Failed to install clangd: ', e);
   const message = `Failed to install clangd language server: ${e}\n` +
-                  "You may want to install it manually.";
+                  'You may want to install it manually.';
   showInstallationHelp(message);
 }
 
@@ -209,8 +209,8 @@ export async function install(release: Github.Release, asset: Github.Asset,
   const extractRoot = path.join(dirs.install, release.tag_name);
   if (await util.promisify(fs.exists)(extractRoot)) {
     const message = `clangd ${release.name} is already installed!`;
-    const use = "Use the installed version";
-    const reinstall = "Delete it and reinstall";
+    const use = 'Use the installed version';
+    const reinstall = 'Delete it and reinstall';
     const response =
         await vscode.window.showInformationMessage(message, use, reinstall);
     if (response == use) {
@@ -232,10 +232,10 @@ export async function install(release: Github.Release, asset: Github.Asset,
   const archive = await unzipper.Open.file(zipFile);
   const executable = findExecutable(archive.files.map(f => f.path));
   await vscode.window.withProgress({
-    location : vscode.ProgressLocation.Notification,
-    title : "Extracting " + asset.name,
+    location: vscode.ProgressLocation.Notification,
+    title: 'Extracting ' + asset.name,
   },
-                                   () => archive.extract({path : extractRoot}))
+                                   () => archive.extract({path: extractRoot}))
   const clangdPath = path.join(extractRoot, executable);
   await fs.promises.chmod(clangdPath, 0o755);
   await fs.promises.unlink(zipFile);
@@ -244,11 +244,11 @@ export async function install(release: Github.Release, asset: Github.Asset,
 
 // Create the 'install' and 'download' directories, and return absolute paths.
 async function createDirs(context: vscode.ExtensionContext) {
-  const install = path.join(context.globalStoragePath, "install");
-  const download = path.join(context.globalStoragePath, "download");
+  const install = path.join(context.globalStoragePath, 'install');
+  const download = path.join(context.globalStoragePath, 'download');
   for (const dir of [install, download])
-    await fs.promises.mkdir(dir, {'recursive' : true});
-  return {install : install, download : download};
+    await fs.promises.mkdir(dir, {'recursive': true});
+  return {install: install, download: download};
 }
 
 // Find the clangd executable within a set of files.
@@ -257,7 +257,7 @@ function findExecutable(paths: string[]): string {
   const entry = paths.find(f => path.posix.basename(f) == filename ||
                                 path.win32.basename(f) == filename);
   if (entry == null)
-    throw new Error("Didn't find a clangd executable!");
+    throw new Error('Didn\'t find a clangd executable!');
   return entry;
 }
 
@@ -265,24 +265,24 @@ function findExecutable(paths: string[]): string {
 // A progress dialog is shown, if it is cancelled then `abort` is signaled.
 async function download(url: string, dest: string,
                         abort: AbortController): Promise<void> {
-  console.log("Downloading ", url, " to ", dest);
+  console.log('Downloading ', url, ' to ', dest);
   const progressOpts = {
-    location : vscode.ProgressLocation.Notification,
-    title : "Downloading " + path.basename(dest),
-    cancellable : true
+    location: vscode.ProgressLocation.Notification,
+    title: 'Downloading ' + path.basename(dest),
+    cancellable: true
   };
   return vscode.window.withProgress(progressOpts, async (progress, cancel) => {
-    const response = await fetch(url, {signal : abort.signal});
+    const response = await fetch(url, {signal: abort.signal});
     if (!response.ok)
       throw new Error(`Failed to download $url`);
     cancel.onCancellationRequested((_) => abort.abort());
     const size = Number(response.headers.get('content-length'));
     let read = 0;
     let fraction = 0;
-    response.body.on("data", (chunk: Buffer) => {
+    response.body.on('data', (chunk: Buffer) => {
       read += chunk.length;
       const newFraction = read / size;
-      progress.report({increment : 100 * (newFraction - fraction)});
+      progress.report({increment: 100 * (newFraction - fraction)});
       fraction = newFraction;
     });
     const out = fs.createWriteStream(dest);
@@ -302,18 +302,18 @@ export async function upgrade(release: Github.Release, clangdPath: string) {
   const installedVer = await installed(clangdPath);
   if (!rangeGreater(releasedVer, installedVer))
     return null;
-  return {old : installedVer.raw, new : releasedVer.raw};
+  return {old: installedVer.raw, new: releasedVer.raw};
 }
 
 // Get the version of an installed clangd binary using `clangd --version`.
 async function installed(clangdPath: string): Promise<semver.Range> {
-  const child = child_process.spawn(clangdPath, [ '--version' ],
-                                    {stdio : [ 'ignore', 'pipe', 'ignore' ]});
+  const child = child_process.spawn(clangdPath, ['--version'],
+                                    {stdio: ['ignore', 'pipe', 'ignore']});
   var output = '';
   for await (const chunk of child.stdout)
     output += chunk;
-  console.log(clangdPath, " --version output: ", output);
-  const prefix = "clangd version ";
+  console.log(clangdPath, ' --version output: ', output);
+  const prefix = 'clangd version ';
   if (!output.startsWith(prefix))
     throw new Error(`Couldn't parse clangd --version output`);
   const rawVersion = output.substr(prefix.length).split(' ', 1)[0];
