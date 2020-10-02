405c9def429d9fb17a1209407d68a64af1859b99
Stefan Haller <stefan@haller-berlin.de>
2020-10-02 16:44:40 +0200

    fixup! Re-read configs when restarting the server

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,6 @@ import * as vscode from 'vscode';
 
 import {ClangdContext} from './clangd-context';
 
-let clangdContext: ClangdContext|null;
-
-async function startClient(globalStoragePath: string,
-                           outputChannel: vscode.OutputChannel) {
-  clangdContext = new ClangdContext;
-  clangdContext.activate(globalStoragePath, outputChannel);
-}
-
-async function stopClient() {
-  await clangdContext?.dispose();
-  clangdContext = null;
-}
-
 /**
  *  This method is called when the extension is activated. The extension is
  *  activated the very first time a command is executed.
@@ -24,17 +11,18 @@ export async function activate(context: vscode.ExtensionContext) {
       vscode.window.createOutputChannel('Clangd Language Server');
   context.subscriptions.push(outputChannel);
 
+  const clangdContext = new ClangdContext;
+  context.subscriptions.push(clangdContext);
+
   // An empty place holder for the activate command, otherwise we'll get an
   // "command is not registered" error.
   context.subscriptions.push(
       vscode.commands.registerCommand('clangd.activate', async () => {}));
   context.subscriptions.push(
       vscode.commands.registerCommand('clangd.restart', async () => {
-        await stopClient();
-        await startClient(context.globalStoragePath, outputChannel);
+        await clangdContext.dispose();
+        await clangdContext.activate(context.globalStoragePath, outputChannel);
       }));
 
-  startClient(context.globalStoragePath, outputChannel);
+  await clangdContext.activate(context.globalStoragePath, outputChannel);
 }
-
-export function deactivate() { stopClient(); }
