c5f6930da95ab6ee38662b1a4ed72dded95951c7
Kirill Bobyrev <3352968+kirillbobyrev@users.noreply.github.com>
2021-01-31 22:54:16 +0300

    Warn about conflict with ms-vscode.cpptools (#141)
    Give a warning when ms-vscode.cpptools extension is enabled and provide an easy way to disable intelliSense (problematic piece that conflicts with our extension) in one click.

--- src/extension.ts
+++ src/extension.ts
@@ -24,4 +24,24 @@ export async function activate(context: vscode.ExtensionContext) {
       }));
 
   await clangdContext.activate(context.globalStoragePath, outputChannel);
+
+  setInterval(function() {
+    const cppTools = vscode.extensions.getExtension('ms-vscode.cpptools');
+    if (cppTools && cppTools.isActive) {
+      const cppToolsConfiguration = vscode.workspace.getConfiguration('C_Cpp');
+      const cppToolsEnabled = cppToolsConfiguration.get('intelliSenseEngine');
+      if (cppToolsEnabled !== 'Disabled') {
+        vscode.window
+            .showWarningMessage(
+                'You have both the Microsoft C++ (cpptools) extension and ' +
+                    'clangd extension enabled. The Microsoft IntelliSense features ' +
+                    'conflict with clangd\'s code completion, diagnostics etc.',
+                'Disable IntelliSense')
+            .then(_ => {
+              cppToolsConfiguration.update('intelliSenseEngine', 'Disabled',
+                                           vscode.ConfigurationTarget.Global);
+            });
+      }
+    }
+  }, 5000);
 }
