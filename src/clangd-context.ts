5776d80520d170fb870e2b30610f5a2b37f95eea
Alex Light <allight@google.com>
2021-02-19 17:48:03 -0800

    Second format fix

--- src/clangd-context.ts
+++ src/clangd-context.ts
@@ -53,9 +53,7 @@ export class ClangdContext implements vscode.Disposable {
     const rootPath: string = vscode.workspace.rootPath;
     const targetCwd: string =
         (rootPath !== undefined) ? rootPath : process.cwd();
-    const clangdExecOptions: vscodelc.ExecutableOptions = {
-      cwd: targetCwd
-    };
+    const clangdExecOptions: vscodelc.ExecutableOptions = {cwd: targetCwd};
     const clangd: vscodelc.Executable = {
       command: clangdPath,
       args: config.get<string[]>('arguments'),
