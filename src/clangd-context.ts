0212d9fe9065a782928776980d6584c9083d2f02
Alex Light <allight@google.com>
2021-02-23 14:12:15 -0800

    Address code review comments to inline changes

--- src/clangd-context.ts
+++ src/clangd-context.ts
@@ -50,14 +50,10 @@ export class ClangdContext implements vscode.Disposable {
     if (!clangdPath)
       return;
 
-    const rootPath: string = vscode.workspace.rootPath;
-    const targetCwd: string =
-        (rootPath !== undefined) ? rootPath : process.cwd();
-    const clangdExecOptions: vscodelc.ExecutableOptions = {cwd: targetCwd};
     const clangd: vscodelc.Executable = {
       command: clangdPath,
       args: config.get<string[]>('arguments'),
-      options: clangdExecOptions
+      options: {cwd: vscode.workspace.rootPath || process.cwd() }
     };
     const traceFile = config.get<string>('trace');
     if (!!traceFile) {
