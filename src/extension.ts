3bce095d76868cb8e64c618b174a95f8e0844251
Nathan James <n.james93@hotmail.co.uk>
2021-04-22 07:05:43 +0100

    Add commands to open global and project config
    Global config opening will likely not work on remote setups and is as of yet untested on windows and mac builds.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,51 @@
+import * as os from 'os'
+import * as path from 'path'
 import * as vscode from 'vscode';
 
 import {ClangdContext} from './clangd-context';
+
+function getConfigFolder(): string {
+  switch (os.platform()) {
+  case 'win32':
+    if (process.env.LocalAppData)
+      return process.env.LocalAppData;
+    break;
+  case 'darwin':
+    if (process.env.HOME)
+      return path.join(process.env.HOME, 'Library', 'Preferences');
+    break;
+  case 'linux':
+    if (process.env.XDG_CONFIG_HOME)
+      return process.env.XDG_CONFIG_HOME;
+    if (process.env.HOME)
+      return path.join(process.env.HOME, '.config');
+    break;
+  default:
+    break;
+  }
+  return null;
+}
+
+function getConfigFile(): string {
+  const Folder = getConfigFolder();
+  if (!Folder)
+    return null;
+  return path.join(Folder, 'clangd', 'config.yaml');
+}
+
+async function openConfigFile(path: vscode.Uri) {
+  let p = path;
+  try {
+    await vscode.workspace.fs.stat(path);
+  } catch {
+    // File doesn't exist, create a scratch file.
+    p = path.with({scheme: 'untitled'});
+  }
+  vscode.workspace.openTextDocument(p).then((a => {
+    vscode.languages.setTextDocumentLanguage(a, 'yaml');
+    vscode.window.showTextDocument(a);
+  }));
+}
 
 /**
  *  This method is called when the extension is activated. The extension is
@@ -22,6 +67,28 @@ export async function activate(context: vscode.ExtensionContext) {
         await clangdContext.dispose();
         await clangdContext.activate(context.globalStoragePath, outputChannel,
                                      context.workspaceState);
+      }));
+
+  // Create a command to open the project root .clangd configuration file.
+  context.subscriptions.push(
+      vscode.commands.registerCommand('clangd.projectSettings', () => {
+        if (vscode.workspace.workspaceFolders) {
+          const folder = vscode.workspace.workspaceFolders[0];
+          openConfigFile(vscode.Uri.joinPath(folder.uri, '.clangd'))
+        } else {
+          vscode.window.showErrorMessage('No project is open');
+        }
+      }));
+
+  context.subscriptions.push(
+      vscode.commands.registerCommand('clangd.globalSettings', () => {
+        const file = getConfigFile();
+        if (file) {
+          openConfigFile(vscode.Uri.file(file));
+        } else {
+          vscode.window.showErrorMessage(
+              'Couldn\'t get global configuration directory');
+        }
       }));
 
   await clangdContext.activate(context.globalStoragePath, outputChannel,
