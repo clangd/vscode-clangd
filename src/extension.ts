275e3dd52a453bd69ac5ee6e89ac51940758f716
Iritscen <iritscen@yahoo.com>
2025-11-17 12:50:00 -0500

    Added option for user to delay the appearance of diagnostics until a fixed time has passed after user inactivty.

--- src/extension.ts
+++ src/extension.ts
@@ -116,6 +116,15 @@ export async function activate(context: vscode.ExtensionContext):
     }, 5000);
   }
 
+  context.subscriptions.push(
+    // Watch for config changes so user can change the diagnostics delay setting at will
+    vscode.workspace.onDidChangeConfiguration((conf) =>
+    {
+      if (conf.affectsConfiguration('clangd.diagnosticsDelay.afterTyping') && clangdContext)
+        clangdContext.updateDelay();
+      })
+  );
+
   apiInstance = new ClangdExtensionImpl(clangdContext?.client);
   return apiInstance;
 }
