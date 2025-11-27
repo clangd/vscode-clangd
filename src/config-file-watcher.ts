8f7df2e14e8e1544bd35eaa0a9ce2404c33c6baa
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-11-27 16:33:27 +0100

    config-file-watcher: Adapt for multiroot workspaces
    We'll only watch the configs for the context specific folder or all
    folders if we have a global context.

--- src/config-file-watcher.ts
+++ src/config-file-watcher.ts
@@ -5,7 +5,8 @@ import {ClangdContext} from './clangd-context';
 import * as config from './config';
 
 export async function activate(context: ClangdContext) {
-  if (await config.get<string>('onConfigChanged') !== 'ignore') {
+  if (await config.get<string>('onConfigChanged', context.workspaceFolder) !==
+      'ignore') {
     context.client.registerFeature(new ConfigFileWatcherFeature(context));
   }
 }
@@ -21,7 +22,8 @@ class ConfigFileWatcherFeature implements vscodelc.StaticFeature {
 
   async initialize(capabilities: vscodelc.ServerCapabilities,
                    _documentSelector: vscodelc.DocumentSelector|undefined) {
-    if (!await config.get<boolean>('onConfigChangedForceEnable') &&
+    if (!await config.get<boolean>('onConfigChangedForceEnable',
+                                   this.context.workspaceFolder) &&
         (capabilities as ClangdClientCapabilities)
             .compilationDatabase?.automaticReload) {
       return;
@@ -43,17 +45,31 @@ class ConfigFileWatcher implements vscode.Disposable {
 
   constructor(private context: ClangdContext) {
     this.createFileSystemWatcher();
-    context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(
-        () => { this.createFileSystemWatcher(); }));
+    // Only watch for workspace folder changes if this is the global context
+    // (no workspaceFolder). Per-folder contexts don't need to update their
+    // watcher since they only watch their specific folder.
+    if (!context.workspaceFolder) {
+      context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(
+          () => { this.createFileSystemWatcher(); }));
+    }
   }
 
   createFileSystemWatcher() {
     if (this.databaseWatcher)
       this.databaseWatcher.dispose();
-    if (vscode.workspace.workspaceFolders) {
+
+    let foldersToWatch: readonly vscode.WorkspaceFolder[];
+    if (this.context.workspaceFolder) {
+      foldersToWatch = [this.context.workspaceFolder];
+    } else if (vscode.workspace.workspaceFolders) {
+      foldersToWatch = vscode.workspace.workspaceFolders;
+    } else {
+      return;
+    }
+
+    if (foldersToWatch.length > 0) {
       this.databaseWatcher = vscode.workspace.createFileSystemWatcher(
-          '{' +
-          vscode.workspace.workspaceFolders.map(f => f.uri.fsPath).join(',') +
+          '{' + foldersToWatch.map(f => f.uri.fsPath).join(',') +
           '}/{build/compile_commands.json,compile_commands.json,compile_flags.txt}');
       this.context.subscriptions.push(this.databaseWatcher.onDidChange(
           this.debouncedHandleConfigFilesChanged.bind(this)));
@@ -82,7 +98,8 @@ class ConfigFileWatcher implements vscode.Disposable {
     if ((await vscode.workspace.fs.stat(uri)).size <= 0)
       return;
 
-    switch (await config.get<string>('onConfigChanged')) {
+    switch (await config.get<string>('onConfigChanged',
+                                     this.context.workspaceFolder)) {
     case 'restart':
       vscode.commands.executeCommand('clangd.restart');
       break;
